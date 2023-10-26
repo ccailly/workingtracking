14c6188ce2e0342fe2278d495c84935c53ad2b67
Clément Cailly <clemcailly@gmail.com>
2023-10-26 13:38:55 +0200

    ✨🗃 Adding break support and quick pick & restructurate data storage

--- README.md
+++ README.md
@@ -7,12 +7,19 @@ Track your working hours and see how much time you have left before the end of y
 - Allow to track your business hours and see how much you have worked in a day.
 - See how much time you have left before the end of your day
 - Lunch break support
+- Break support
 
 ## Known Issues
 
 Calling out known issues can help limit users opening duplicate issues against your extension.
 
 ## Release Notes
+
+### 1.2.0
+
+- FEATURE - Add a new commands to manage breaks
+- FEATURE - Quick pick command (access to the commands from the status bar)
+- CHORE - Restructure the data storage
 
 ### 1.1.1
 
--- src/extension.ts
+++ src/extension.ts
@@ -6,16 +6,70 @@ export function activate(context: vscode.ExtensionContext) {
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
 	updateStatusBarItem(context);
 
+	let disposableQuickPick = vscode.commands.registerCommand('working-tracking.quickPick', () => {
+		const items: vscode.QuickPickItem[] = [
+			{
+				label: 'Définir heure d\'arrivée',
+				description: 'Définir votre heure d\'arrivée'
+			},
+			{
+				label: 'Définir heure de début de pause',
+				description: 'Définir votre heure de début de pause'
+			},
+			{
+				label: 'Définir heure de fin de pause',
+				description: 'Définir votre heure de fin de pause'
+			},
+			{
+				label: 'Démarrer une pause',
+				description: 'Démarrer une pause'
+			},
+			{
+				label: 'Terminer la dernière pause',
+				description: 'Terminer la dernière pause'
+			},
+			{
+				label: 'Ouvrir le dashboard',
+				description: 'Ouvrir le dashboard'
+			}
+		];
+
+		vscode.window.showQuickPick(items).then((item) => {
+			if (item) {
+				switch (item.label) {
+					case 'Définir heure d\'arrivée':
+						vscode.commands.executeCommand('working-tracking.setArrivalTime');
+						break;
+					case 'Définir heure de début de pause':
+						vscode.commands.executeCommand('working-tracking.setLunchStartTime');
+						break;
+					case 'Définir heure de fin de pause':
+						vscode.commands.executeCommand('working-tracking.setLunchEndTime');
+						break;
+					case 'Démarrer une pause':
+						vscode.commands.executeCommand('working-tracking.startBreak');
+						break;
+					case 'Terminer la dernière pause':
+						vscode.commands.executeCommand('working-tracking.endBreak');
+						break;
+					case 'Ouvrir le dashboard':
+						vscode.commands.executeCommand('working-tracking.openDashboard');
+						break;
+				}
+			}
+		});
+	});
+
 	let disposableArrival = vscode.commands.registerCommand('working-tracking.setArrivalTime', () => {
 		vscode.window.showInputBox({ prompt: "Entrez votre heure d'arrivée (format HH:mm)" }).then((input) => {
 			if (input) {
-                let date = new Date();
-                date.setHours(input?.split(':')[0] as any);
-                date.setMinutes(input?.split(':')[1] as any);
+				let date = new Date();
+				date.setHours(input?.split(':')[0] as any);
+				date.setMinutes(input?.split(':')[1] as any);
 				date.setSeconds(0);
 
 				setDailyArrival(context, date.getTime());
-                updateStatusBarItem(context);
+				updateStatusBarItem(context);
 				vscode.window.showInformationMessage('Votre heure d\'arrivée a été redéfinie à ' + date.toLocaleTimeString());
 			}
 		});
@@ -51,17 +105,37 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 	});
 
+	let disposableStartBreak = vscode.commands.registerCommand('working-tracking.startBreak', () => {
+		startBreak(context);
+		updateStatusBarItem(context);
+		vscode.window.showInformationMessage('Votre pause a été démarrée');
+	});
+
+	let disposableEndBreak = vscode.commands.registerCommand('working-tracking.endBreak', () => {
+		endBreak(context);
+		updateStatusBarItem(context);
+		vscode.window.showInformationMessage('Votre pause a été terminée');
+	});
+
 	const updateInterval = setInterval(() => {
 		updateStatusBarItem(context);
 	}, 1000 * 60);
-	context.subscriptions.push(disposableArrival, disposableLunchStart, disposableLunchEnd);
+	context.subscriptions.push(disposableQuickPick, disposableArrival, disposableLunchStart, disposableLunchEnd, disposableStartBreak, disposableEndBreak);
 	context.subscriptions.push(statusBarItem);
 
 	// Push the interval to the subscriptions so it gets cleared on deactivation
-    context.subscriptions.push({ dispose: () => clearInterval(updateInterval) });
+	context.subscriptions.push({ dispose: () => clearInterval(updateInterval) });
 }
 
-export function deactivate() { }
+export function deactivate(context: vscode.ExtensionContext) {
+	// Clear the interval on deactivation
+	context.subscriptions.forEach((subscription) => {
+		subscription.dispose();
+	});
+
+	// Set departure time
+	setDeparture(context);
+}
 
 /**
  * Update the status bar item
@@ -70,43 +144,30 @@ export function deactivate() { }
  */
 function updateStatusBarItem(context: vscode.ExtensionContext) {
 	const dailyArrival = getDailyArrival(context);
-    let arrival = new Date(dailyArrival).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    let dailyDeparture = getDailyDeparture(context);
-    let lunchDuration = getLunchDuration(context);
-    let timeLeft = dailyDeparture - Date.now() + lunchDuration;
+
+	let arrival = new Date(dailyArrival).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+	let dailyDeparture = getDailyDeparture(context);
+	let lunchDuration = getLunchDuration(context);
+	let timeLeft = dailyDeparture - Date.now() + lunchDuration;
+
 	let sign = timeLeft < 0 ? '+' : '';
 	timeLeft = Math.abs(timeLeft);
 	let hours = Math.floor(timeLeft / (60 * 60 * 1000));
 	let minutes = Math.floor((timeLeft % (60 * 60 * 1000)) / (60 * 1000));
 
 	// Compute status bar background color
-	if (timeLeft < 0) {
-		statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.debuggingBackground');
-	} else if (getLunchEnd(context) === undefined) {
-		statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.warningBackground');
-	} else {
-		statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.background');
-	}
+	// if (timeLeft !== null && timeLeft < 0) {
+	// 	statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.debuggingBackground');
+	// } else if (getLunchEnd(context) === undefined) {
+	// 	statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.warningBackground');
+	// } else {
+	// 	statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.background');
+	// }
 
 	statusBarItem.text = '$(timeline-open) ' + sign + hours + ' hrs ' + minutes + ' mins';
-	statusBarItem.command = 'working-tracking.openDashboard';
-	statusBarItem.tooltip = 'Work since ' + arrival + ' today - Click to visit dashboard';
+	statusBarItem.command = 'working-tracking.quickPick';
+	statusBarItem.tooltip = 'Work since ' + arrival + ' today - Click to open Quick Pick';
 	statusBarItem.show();
-}
-
-/**
- * Return the daily arrival time in milliseconds
- *
- * @param context
- * @return number The daily arrival time in milliseconds
- */
-function getDailyArrival(context: vscode.ExtensionContext) {
-	const dailyArrival = context.globalState.get(new Date().toLocaleDateString());
-	if (dailyArrival && typeof dailyArrival === 'number') {
-		return dailyArrival;
-	}
-
-	return setDailyArrival(context)
 }
 
 /**
@@ -120,25 +181,6 @@ function getDailyDeparture(context: vscode.ExtensionContext) {
 	return dailyArrival + 7 * 60 * 60 * 1000;
 }
 
-
-/**
- * Set the daily arrival time in milliseconds
- *
- * @param context
- * @return number The daily arrival time in milliseconds
- */
-function setDailyArrival(context: vscode.ExtensionContext, arrivalTime?: number) {
-    if (!arrivalTime) {
-        arrivalTime = Date.now();
-    }
-
-	context.globalState.update(
-		new Date().toLocaleDateString(),
-		new Date(arrivalTime).getTime()
-	);
-	return Date.now();
-}
-
 /**
  * Return the lunch duration in milliseconds
  *
@@ -146,57 +188,152 @@ function setDailyArrival(context: vscode.ExtensionContext, arrivalTime?: number)
  * @return number The lunch duration in milliseconds
  */
 function getLunchDuration(context: vscode.ExtensionContext) {
-	const lunchStart = context.globalState.get(new Date().toLocaleDateString() + '-lunch-start');
-	const lunchEnd = context.globalState.get(new Date().toLocaleDateString() + '-lunch-end');
+	const lunchStart = getLunchStart(context);
+	const lunchEnd = getLunchEnd(context);
 	if (lunchStart && lunchEnd) {
-		return (lunchEnd as number) - (lunchStart as number);
+		return lunchEnd - lunchStart;
 	}
 
 	return 60 * 60 * 1000; // default to 1 hour
 }
 
-/**
- * Return the lunch start time in milliseconds
- *
- * @param context
- * @return number The lunch start time in milliseconds
- */
-function getLunchStart(context: vscode.ExtensionContext) {
-	return context.globalState.get(new Date().toLocaleDateString() + '-lunch-start');
+interface DailyData {
+	arrival: number;
+	departure: number | null;
+	lunchStart: number | null;
+	lunchEnd: number | null;
+	breaks: Break[];
+}
+
+interface Break {
+	start: number;
+	end: number | null;
 }
 
 /**
- * Return the lunch end time in milliseconds
+ * Return the daily data
  *
  * @param context
- * @return number The lunch end time in milliseconds
+ * @return DailyData
  */
-function getLunchEnd(context: vscode.ExtensionContext) {
-	return context.globalState.get(new Date().toLocaleDateString() + '-lunch-end');
+function getTodayData(context: vscode.ExtensionContext): DailyData {
+	const todayData = context.globalState.get<DailyData>(new Date().toLocaleDateString());
+	if (todayData) {
+		return todayData;
+	}
+
+	const defaultData: DailyData = {
+		arrival: Date.now(),
+		departure: null,
+		lunchStart: null,
+		lunchEnd: null,
+		breaks: []
+	};
+
+	context.globalState.update(new Date().toLocaleDateString(), defaultData);
+	return defaultData;
 }
 
 /**
- * Set the lunch start time in milliseconds
+ * Set the daily arrival time
+ *
+ * @param context
+ * @param arrivalTime
+ */
+function setDailyArrival(context: vscode.ExtensionContext, arrivalTime?: number) {
+	const todayData = getTodayData(context);
+	todayData.arrival = arrivalTime || Date.now();
+	context.globalState.update(new Date().toLocaleDateString(), todayData);
+}
+
+/**
+ * Set the daily departure time
+ *
+ * @param context
+ * @param departureTime
+ */
+function setDeparture(context: vscode.ExtensionContext, departureTime?: number) {
+	const todayData = getTodayData(context);
+	todayData.departure = departureTime || Date.now();
+	context.globalState.update(new Date().toLocaleDateString(), todayData);
+}
+
+/**
+ * Set the lunch start time
  *
  * @param context
  * @param lunchStart
  */
 function setLunchStart(context: vscode.ExtensionContext, lunchStart: number) {
-	context.globalState.update(
-		new Date().toLocaleDateString() + '-lunch-start',
-		lunchStart
-	);
+	const todayData = getTodayData(context);
+	todayData.lunchStart = lunchStart;
+	context.globalState.update(new Date().toLocaleDateString(), todayData);
 }
 
 /**
- * Set the lunch end time in milliseconds
+ * Set the lunch end time
  *
  * @param context
  * @param lunchEnd
  */
 function setLunchEnd(context: vscode.ExtensionContext, lunchEnd: number) {
-	context.globalState.update(
-		new Date().toLocaleDateString() + '-lunch-end',
-		lunchEnd
-	);
+	const todayData = getTodayData(context);
+	todayData.lunchEnd = lunchEnd;
+	context.globalState.update(new Date().toLocaleDateString(), todayData);
+}
+
+/**
+ * Return the daily departure time
+ *
+ * @param context
+ * @returns
+ */
+function getDailyArrival(context: vscode.ExtensionContext) {
+	return getTodayData(context).arrival;
+}
+
+/**
+ * Return the daily departure time
+ *
+ * @param context
+ * @returns
+ */
+function getLunchStart(context: vscode.ExtensionContext) {
+	return getTodayData(context).lunchStart;
+}
+
+/**
+ * Return the daily departure time
+ *
+ * @param context
+ * @returns
+ */
+function getLunchEnd(context: vscode.ExtensionContext) {
+	return getTodayData(context).lunchEnd;
+}
+
+/**
+ * Start a break
+ *
+ * @param context
+ */
+function startBreak(context: vscode.ExtensionContext) {
+	const todayData = getTodayData(context);
+	todayData.breaks.push({ start: Date.now(), end: null });
+	context.globalState.update(new Date().toLocaleDateString(), todayData);
+}
+
+/**
+ * End the last break
+ *
+ * @param context
+ */
+function endBreak(context: vscode.ExtensionContext) {
+	const todayData = getTodayData(context);
+	const lastBreak = todayData.breaks.pop();
+	if (lastBreak && lastBreak.end === null) {
+		lastBreak.end = Date.now();
+		todayData.breaks.push(lastBreak);
+		context.globalState.update(new Date().toLocaleDateString(), todayData);
+	}
 }
